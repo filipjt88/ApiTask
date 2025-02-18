5eed70f6a1e455c74e2368001ea482e450656899
filipjt88 <filip88ks@gmail.com>
2025-02-18 12:59:31 +0100

    save

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const taskList = document.getElementById("taskList");
+const newTaskInput = document.getElementById("newTaskInput");
+const addTaskBtn = document.getElementById("addTask");
+
+function loadTasks() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+        .then(response => response.json())
+        .then(tasks => {
+            taskList.innerHTML = "";
+            tasks.forEach(task => addTaskToDOM(task));
+        }).catch(error => console.error("Doslo je do greske pri dodavanju novog podatka", error));
+}
